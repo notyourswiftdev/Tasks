e23b515eb476587f7023960d6ea78cb12a6ba690
Aaron Cleveland <toscleveland@gmail.com>
2020-01-27 11:52:53 -0600

    CoreData Init

---
+++ Tasks/Controllers/Model Controllers/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Tasks
+//
+//  Created by Aaron Cleveland on 1/27/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import Foundation
---
+++ Tasks/Controllers/View Controllers/TaskDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Aaron Cleveland on 1/27/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
